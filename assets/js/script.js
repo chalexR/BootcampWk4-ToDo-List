fbf8f559677dcf845e9926f91f130bf9e803cb80
Charles Richer <chalex.richer@gmail.com>
2025-07-08 11:22:37 +0100

    added comments to everything

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // ToDo List Scripts
-// Version 0.2
+// Version 1.0
 
 var taskArray = [{
     id : 1,
@@ -44,58 +44,72 @@ var taskArray = [{
     completed : false
 }];
 
-// FUNCTION to load tasks from array
+// FUNCTION to load tasks from array - set the default display condition to incomplete, this will mean by default we see the incomplete tasks
 function loadTasks(condition = "incomplete"){
-   // define the element we are changing
-    const taskBodyEl = document.getElementById("taskBody");
-    taskBodyEl.innerHTML = "";
+    
+    // define the element we are changing
+    const taskBodyEl = document.getElementById("taskBody")
+    // this is required to reset the taskBody content. Without it the script loads each filter underneath the old data set
+    taskBodyEl.innerHTML = ""
+    
+    // loop through the entire taskArray
     for (i=0; i < taskArray.length; i++){
         
-        //define variables for use in the for statement
+        // define variables for use in the for statement
         let taskStatus = taskArray[i].status;
+        // skip any task that doesnt match the display condition
+        if(condition != taskStatus && condition != false){continue;}
+        // continue defining variables
         let taskContent = taskArray[i].content;
         let taskCreated = taskArray[i].created;
         let taskID = taskArray[i].id;
-        if(condition != taskStatus && condition != false){continue;}
 
-        // set a variable for the HTML that will be included
+        // set a variable for the internal table structure HTML that will be included
         let innerHTML = `
             <td>${taskStatus}</td>
             <td>${taskContent}</td>
             <td>${taskCreated}</td>
             <td><button class="complete-btn" onclick="completeTask(${taskID})">✅</button><button class="complete-btn" onclick="editTask(${taskID})">✍🏻</button></td>
         `;
+
+        // set variable to start building the table internals
         let newTaskEl = document.createElement("tr");
+        // apply the internal table structure
         newTaskEl.innerHTML = innerHTML;
+        // append the structure to the table
         taskBodyEl.appendChild(newTaskEl);
     }
-
 }
 
+// FUNCTION to load a task to edit into the form
 function editTask(id){
     //cycle through tasks in the Array
     for (i=0; i<taskArray.length; i++){
         //check to see if the current task ID equals the id we are trying to change
         if (taskArray[i].id == id){
-            // set a variable for the taskInput
+            // set a variable for the text taskInput
             var taskInputEl = document.getElementById("newTaskInput");
+            // set a variable for the taskID input, we use this to target the correct task to edit
             var taskIdInputEl = document.getElementById("taskEditID");
             // set a variable for the taskButton
             const taskButton = document.getElementById("newTaskButton");
             // set the task Input to the value of the task content we're editting
             taskInputEl.value = taskArray[i].content;
+            // set the taskID Input to the ID of the task we are editing 
             taskIdInputEl.value = id;
-            // set a clas sin the button that can be checked in the event listener
+            // set a class in the button that can be checked in the event listener
             taskButton.classList.add('task-edit');
+            // change the button text to edit
             taskButton.innerHTML = "Edit Task";
         }
     }
 }
 
+// FUNCTION to complete a task on button click
 function completeTask(id){
-    //cycle through tasks in the Array
+    // cycle through tasks in the Array
     for (i=0; i<taskArray.length; i++){
-        //check to see if the current task ID equals the id we are trying to change
+        // check to see if the current task ID equals the id we are trying to change
         if (taskArray[i].id == id){
             //change the status to complete
             taskArray[i].status = "complete";
@@ -107,16 +121,22 @@ function completeTask(id){
     //console.log(taskArray);
 }
 
+// FUNCTION to find the current highest ID 
 function highestID(){
+    //set variable to use as counter
     let max = 0;
+    // cycle through the taskArray
     for (i=0; i<taskArray.length; i++){
-        currentID = parseInt(taskArray[i].id);
+        // set variable for the current records ID
+        let currentID = parseInt(taskArray[i].id);
+        // if this records ID is bigger than the current stored max number then chanfge the max to the current number
         if (currentID > max){ max = currentID}
     }
+    //return the maximum value found
     return max;
 }
 
-// function to add a new task to the list
+// FUNCTION to add a new task to the list OR edit an existing one. Task Edit is set to false as default
 function addTask(edit = false){
 
     //set a variable that grabs the content from the new task input
@@ -128,8 +148,9 @@ function addTask(edit = false){
 
         // set a variable find an element of the taskArray that equals the taskInput
         const dupeCheck = taskArray.find((element) => element.content === taskInputEl);
-        console.log(dupeCheck);
-        // if the dupeCheck has found something and we are not in edit mode the send an alert
+        //console.log(dupeCheck);
+
+        // if the dupeCheck has found something and we are not in edit mode then send an alert
         if(dupeCheck != undefined && !edit) {alert("Duplicate Task!");}else{
             
             // set constant for the area of html we will be adding the new task to. 
@@ -147,7 +168,7 @@ function addTask(edit = false){
             // set a formatted date variable
             const formattedDate = `${yyyy}/${mm}/${dd} ${time}`;
 
-            //check if we are not in edit mode
+            //check that we are not in edit mode
             if(!edit){
                 // if we are not in edit mode then we can use a new ID
                 taskID = highestID() + 1;
@@ -162,25 +183,35 @@ function addTask(edit = false){
                 // push the new array to the end of our existing task list
                 taskArray.push(addTaskArray);
             }else{
-                //if we are in edit mode then use the ID from the dupecheck we did earlier
+                //if we are in edit mode then use the ID from the taskEdit Input value
                 taskID = parseInt(document.getElementById("taskEditID").value);
+
                 // Find the index of the task we want to edit
                 const editIndex = taskArray.findIndex(task => task.id === taskID);
 
+                // check the Index doesnt equal -1 (not found)
                 if (editIndex !== -1) {
+                    // change the content entry for the array entry with the supplied ID
                     taskArray[editIndex].content = taskInputEl;
+                // throw an error if we can;t find the correct index
                 }else{alert("Error: Could not find the record to edit!")}
+
+                // reset the Edit ID
                 document.getElementById("taskEditID").value = "";
+                // reset the task button class
                 document.getElementById("newTaskButton").classList.remove("task-edit");
+                // reset the task button text
                 document.getElementById("newTaskButton").innerHTML = "Add Task";
             }
 
+            // rerun the load task
             loadTasks();
+            // reset the task text input
             document.getElementById("newTaskInput").value = "";
+            // focus the cursor on the task input 
             document.getElementById("newTaskInput").focus();
-            console.log(taskArray);
+            //console.log(taskArray);
         }
-
     }
 }
 
